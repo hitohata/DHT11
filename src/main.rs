def4be400b7f1e4d6bcefa4ecab79a9930ddfe6b
hirohata <hirohatatro@gmail.com>
2023-06-06 08:45:22 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use core::panic;
 
-use rppal::gpio::{ Gpio, IoPin, Level, Mode };
+use rppal::gpio::{ Gpio, IoPin, Level, Mode, PullUpDown };
+use rppal::hal::Delay;
+use embedded_hal::blocking::delay::{ DelayMs, DelayUs };
 
 const GPIO_COMMUNICATOR_PIN: u8 = 5;
 const TIME_OUT_USEC: u16 = 1000;
 
+
 fn main() {
 
     let mut dht11 = DHT11::new(GPIO_COMMUNICATOR_PIN);
+    let mut delay = Delay::new();
 
     let mut buf : Vec<u8>= vec![];
 
-    dht11.setup();
+    dht11.setup(&mut delay);
 
-    for i in 0..40 {
-        println!("{}: {:?}", i + 1, dht11.read_bit());
+    for _ in 0..40 {
+        print!("{} ", if dht11.read_bit(&mut delay) { 1 } else { 0 });
     }
 
 }
 
 struct DHT11
 {
-    pin: IoPin
+    pin: IoPin,
 }
 
 impl DHT11 {
@@ -29,26 +33,39 @@ impl DHT11 {
 
         let gpio = Gpio::new().unwrap();
         let pin = gpio.get(pin_number).unwrap();
+        let mut io_pin = pin.into_io(Mode::Input);
+        io_pin.set_pullupdown(PullUpDown::PullUp);
 
-        DHT11 { pin: pin.into_io(Mode::Input) }
+        DHT11 { pin: io_pin }
     }
 
     /// Set up the connection to the DHT11
-    pub fn setup(&mut self) {
+    pub fn setup<D>(&mut self, delay: &mut D)
+        where D: DelayMs<u16> + DelayUs<u16>
+    {
 
         // send a start signal.
         self.to_output_mode();
+        self.set_high_level();
+
+        delay.delay_ms(10);
+
         self.set_low_level();
-        self.wait_mil_sec(20);
+
+        delay.delay_ms(20);
 
         self.to_input_mode();
-        self.wait_micro_sec(40);
-        self.read_bit();
+
+        delay.delay_us(10);
+
+        println!("after handshake: {}", self.read_bit(delay));
     }
 
-    fn read_bit(&mut self) -> bool {
-        let low_time = self.wait_pulse(Level::High);
-        let high_time = self.wait_pulse(Level::Low);
+    fn read_bit<D>(&mut self, delay: &mut D) -> bool
+        where D: DelayMs<u16> + DelayUs<u16>
+    {
+        let low_time = self.wait_pulse(Level::High, delay);
+        let high_time = self.wait_pulse(Level::Low, delay);
 
         println!("low: {}, high: {}", low_time, high_time);
 
@@ -56,22 +73,23 @@ impl DHT11 {
     }
 
     /// wait until it changes to the disired voltage.
-    fn wait_pulse(&mut self, level: Level) -> u32 {
+    fn wait_pulse<D>(&mut self, level: Level, delay: &mut D) -> u32
+        where D: DelayMs<u16> + DelayUs<u16>
+    {
 
         if self.pin.mode() != Mode::Input {
             self.to_input_mode();
         }
 
-        let mut count = 0_u16;
-
-        println!("{:?}", self.pin.read());
+        let mut count = 0;
 
         while self.pin.read() != level {
+            println!("{:?}: {}",self.pin.read(), count);
             count += 1;
             if count > TIME_OUT_USEC {
                 panic!("time out"); // handle
             }
-            self.wait_micro_sec(1);
+            delay.delay_us(1);
         }
 
         u32::from(count)
@@ -92,7 +110,15 @@ impl DHT11 {
         if self.pin.mode() != Mode::Output {
             self.to_output_mode();
         }
-        self.set_low_level();
+        self.pin.set_low();
+    }
+
+    /// set an output pin level to low.
+    fn set_high_level(&mut self) {
+        if self.pin.mode() != Mode::Output {
+            self.to_output_mode();
+        }
+        self.pin.set_high();
     }
 
     #[inline]
