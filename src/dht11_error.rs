f46b77b45690740b97b6bc0eafdbf8cc8a30d447
hirohata <hirohatatro@gmail.com>
2023-06-09 02:48:46 +0100

    creation complete

--- src/dht11_error.rs
+++ src/dht11_error.rs
@@ -4,4 +4,10 @@ use thiserror::Error;
 pub enum DHT11Error {
     #[error("DHT11 initialization failed. {0}")]
     DHT11InitializationError(String),   
+    #[error("Sensor Time Out")]
+    SensorReadingTimeOut,
+    #[error("Checksum error")]
+    ChecksumError,
+    #[error(transparent)]
+    FloatParseError(#[from] std::num::ParseFloatError)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use core::{panic};
+mod dht11_error;
+
 use std::fmt;
 use rppal::gpio::{ Gpio, IoPin, Level, Mode, PullUpDown };
 use rppal::hal::Delay;
 use embedded_hal::blocking::delay::{ DelayMs, DelayUs };
+use dht11_error::DHT11Error;
 
 const GPIO_COMMUNICATOR_PIN: u8 = 5;
 const TIME_OUT_USEC: u16 = 1000;
+const RETRY: u8 = 3;
 
 
 fn main() {
 
-    let mut dht11 = DHT11::new(GPIO_COMMUNICATOR_PIN);
+    let mut dht11 = DHT11::new(GPIO_COMMUNICATOR_PIN).unwrap();
     let mut delay = Delay::new();
 
-    dht11.setup(&mut delay);
+    let result = dht11.read(&mut delay);
 
-    let result =dht11.read(&mut delay);
 
     println!("{:?}", result);
 
@@ -28,8 +30,8 @@ struct DHT11
 
 #[derive(Debug)]
 struct MeasurementResult {
-    temperature: f32,
-    humidity: f32
+    pub temperature: f32,
+    pub humidity: f32
 }
 
 impl fmt::Display for MeasurementResult {
@@ -39,24 +41,82 @@ impl fmt::Display for MeasurementResult {
 }
 
 impl DHT11 {
-    pub fn new(pin_number: u8) -> Self {
+    pub fn new(pin_number: u8) -> Result<Self, DHT11Error> {
 
-        let gpio = Gpio::new().unwrap();
-        let pin = gpio.get(pin_number).unwrap();
+        let gpio = match Gpio::new() {
+            Ok(gpio) => gpio,
+            Err(_) => return Err(DHT11Error::DHT11InitializationError("GPIO initialization failed.".to_string()))
+        };
+
+        let pin = match gpio.get(pin_number) {
+            Ok(pin) => pin,
+            Err(_) => return Err(DHT11Error::DHT11InitializationError(format!("Pin, {}, initialization failed", pin_number)))
+        };
+
         let mut io_pin = pin.into_io(Mode::Input);
         io_pin.set_pullupdown(PullUpDown::PullUp);
 
-        DHT11 { pin: io_pin }
+        Ok(DHT11 { pin: io_pin })
     }
 
-    pub fn read<D>(&mut self, delay: &mut D) -> MeasurementResult
+    pub fn read<D>(&mut self, delay: &mut D) -> Result<MeasurementResult, DHT11Error>
         where D: DelayMs<u16> + DelayUs<u16>
     {
 
-        let mut buffer = vec![0_u8; 5];
+        let mut i = 1;
+
+        let measurement_result: MeasurementResult = loop {
+
+            match self.setup(delay) {
+                Ok(_) => (),
+                Err(err) => {
+                    if i > RETRY {
+                        return Err(err)
+                    } else {
+                        i += 1;
+                        continue;
+                    }
+                }
+            } 
+
+            match self.read_data(delay) {
+                Err(err) => {
+                    if i > RETRY {
+                        return Err(err)
+                    } else {
+                        i += 1;
+                        continue;
+                    }
+                },
+                Ok(data) => {
+                    let humidity = format!("{}.{}", data[0], data[1]).parse::<f32>()?;
+                    let temperature = format!("{}.{}", data[2], data[3]).parse::<f32>()?;
+                    break MeasurementResult{
+                        temperature, humidity
+                    };
+
+                }
+            }
+        };
+    
+        Ok(measurement_result)
+    }
+
+
+
+    /// read the sensor data. 
+    /// /[0/]: humidity integral
+    /// /[1/]: humidity decimal
+    /// /[2/]: temperature integral
+    /// /[3/]: temperature decimal
+    /// /[4/]: checksum
+    fn read_data<D>(&mut self, delay: &mut D) -> Result<[u8; 5], DHT11Error>
+        where D: DelayMs<u16> + DelayUs<u16>
+    {
+        let mut buffer = [0_u8; 5];
 
         for i in 0..40 {
-            if self.read_bit(delay) {
+            if self.read_bit(delay)? {
                 buffer[i / 8] |= 1 << ((7 - i % 8));
             }
         }
@@ -66,21 +126,15 @@ impl DHT11 {
             .wrapping_add(buffer[2])
             .wrapping_add(buffer[3]);
 
-        if checksum != buffer[4] {
-            println!("CHECK SUM ERROR"); // TODO: implement error
-        } 
-
-        let humidity = format!("{}.{}", buffer[0], buffer[1]).parse::<f32>().unwrap();
-        let temperature = format!("{}.{}", buffer[2], buffer[3]).parse::<f32>().unwrap();
-
-        MeasurementResult {
-            temperature,
-            humidity
+        if checksum == buffer[4] {
+            Ok(buffer)
+        } else {
+            Err(DHT11Error::ChecksumError)
         }
     }
 
     /// Set up the connection to the DHT11
-    pub fn setup<D>(&mut self, delay: &mut D)
+    fn setup<D>(&mut self, delay: &mut D) -> Result<(), DHT11Error>
         where D: DelayMs<u16> + DelayUs<u16>
     {
 
@@ -95,20 +149,22 @@ impl DHT11 {
         self.to_input_mode();
         delay.delay_us(20);
 
-        self.read_bit(delay);
+        self.read_bit(delay)?;
+        
+        Ok(())
     }
 
-    fn read_bit<D>(&mut self, delay: &mut D) -> bool
+    fn read_bit<D>(&mut self, delay: &mut D) -> Result<bool, DHT11Error>
         where D: DelayMs<u16> + DelayUs<u16>
     {
-        let low_time = self.wait_pulse(Level::High, delay);
-        let high_time = self.wait_pulse(Level::Low, delay);
+        let low_time = self.wait_pulse(Level::High, delay)?;
+        let high_time = self.wait_pulse(Level::Low, delay)?;
 
-        high_time > low_time
+        Ok(high_time > low_time)
     }
 
     /// wait until it changes to the disired voltage.
-    fn wait_pulse<D>(&mut self, level: Level, delay: &mut D) -> u32
+    fn wait_pulse<D>(&mut self, level: Level, delay: &mut D) -> Result<u32, DHT11Error>
         where D: DelayMs<u16> + DelayUs<u16>
     {
 
@@ -121,12 +177,12 @@ impl DHT11 {
         while self.pin.read() != level {
             count += 1;
             if count > TIME_OUT_USEC {
-                panic!("time out"); // handle
+                return Err(DHT11Error::SensorReadingTimeOut)
             }
             delay.delay_us(1);
         }
 
-        u32::from(count)
+        Ok(u32::from(count))
     }
 
     /// change a pin to output.
